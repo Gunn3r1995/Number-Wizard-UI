4877399eaa4bd9e0f86807e8878bd953369d4a5e
Shane Smith <srs23@brighton.ac.uk>
2017-12-05 13:43:51 +0000

    initial commit

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts
+{
+    public class LevelManager : MonoBehaviour {
+
+        public void LoadLevel(string sceneName) {
+            Debug.Log("Level load requested for: " + sceneName);
+            SceneManager.LoadScene(sceneName);
+        }
+
+        public void QuitRequest()
+        {
+            Debug.Log("I want to quit!");
+            Application.Quit();
+        }
+    }
+}
---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Assets.Scripts
+{
+    public class NumberWizard : MonoBehaviour
+    {
+        private int _guess;
+
+        public int min = 1;
+        public int max = 1000;
+        public int _maxGuessesAllowed = 10;
+        public Text GuessText;
+
+        // Use this for initialization
+        void Start()
+        {
+            StartGame();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                GuessHigher();
+            }
+            else if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                GuessLower();
+            }
+            else if (Input.GetKeyDown(KeyCode.Return))
+            {
+                SceneManager.LoadScene("Lose");
+            }
+        }
+        
+        private void StartGame()
+        {
+            _guess = Random.Range(min, max);
+            GuessText.text = _guess.ToString();
+            max++;
+        }
+        
+        public void GuessLower()
+        {
+            max = _guess;
+            NextGuess();
+        }
+
+        public void GuessHigher()
+        {
+            min = _guess;
+            NextGuess();
+        }
+
+        private void NextGuess()
+        {
+            _guess = Random.Range(min, max);
+            GuessText.text = _guess.ToString();
+            _maxGuessesAllowed--;
+            if (_maxGuessesAllowed <= 0)
+            {
+                SceneManager.LoadScene("Win");
+            }
+        }
+    }
+}
